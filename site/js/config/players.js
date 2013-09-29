c5adb6730044f434c96ff926d4c3b3e4c6c29a58
eran_co <shush6@gmail.com>
2013-09-29 11:24:21 +0300

    table design

--- site/js/config/players.js
+++ site/js/config/players.js
@@ -5,42 +5,42 @@ define(function(){
             seat:1,
             name: 'vick7',
             balance: 6363,
-            imageUrl:'../player.jpg'
+            imageUrl:'../img/faces/1.jpg'
         },
         {
             id: 2,
             seat:2,
             name: 'adam',
             balance: 6265,
-            imageUrl:'../player.jpg'
+            imageUrl:'../img/faces/2.jpg'
         },
         {
             id: 4,
             seat:3,
             name: 'seycat',
             balance: 9620,
-            imageUrl:'../player.jpg'
+            imageUrl:'../img/faces/3.jpg'
         },
         {
             id: 7,
             seat:4,
             name: 'ddiedem',
             balance: 417,
-            imageUrl:'../player.jpg'
+            imageUrl:'../img/faces/1.jpg'
         },
         {
             id: 11,
             seat:5,
             name: 'renzo1',
             balance: 23589,
-            imageUrl:'../player.jpg'
+            imageUrl:'../img/faces/2.jpg'
         },
         {
             id: 97,
             seat:6,
             name: 'icklokobo',
             balance: 7852,
-            imageUrl:'../player.jpg'
+            imageUrl:'../img/faces/3.jpg'
         }
     ]
 
