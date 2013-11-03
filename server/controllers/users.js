c8153cd6983e5a1f70e38ce04774bc89adbbef1f
eran_co <shush6@gmail.com>
2013-11-03 07:20:03 +0200

    jshind fixes

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,30 +2,30 @@
 var passport = require('passport'),
     User = require('../models/user').userModel;
 
-exports.getLoginPage = function(req, res){
+exports.getLoginPage = function (req, res) {
     res.sendfile('site/login.html');
 };
 
-exports.login = function(req, res, next) {
-    passport.authenticate('local', function(err, user, info) {
+exports.login = function (req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
         if (err) { return next(err); }
         if (!user) {
             req.session.messages =  [info.message];
-            return res.send({res:false});
+            return res.send({res: false});
         }
-        req.logIn(user, function(err) {
+        req.logIn(user, function (err) {
             if (err) { return next(err); }
-            return res.send({res:true});
+            return res.send({res: true});
         });
     })(req, res, next);
 };
 
-exports.register = function(req, res) {
+exports.register = function (req, res) {
     var userName = req.body.username;
     var password = req.body.password;
 
-    if (!userName || !password){
-        res.send({res:false, message: "user name and password can't be empty"});
+    if (!userName || !password) {
+        res.send({res: false, message: "user name and password can't be empty"});
     }
 
     var user = new User({
@@ -45,20 +45,20 @@ exports.register = function(req, res) {
             else {
                 message = 'please try again';
             }
-            res.send({res:false, message: message});
+            res.send({res: false, message: message});
         }
         else {
-            res.send({res:true});
+            res.send({res: true});
         }
     });
 };
 
-exports.logout = function(req, res) {
+exports.logout = function (req, res) {
     req.logout();
     res.send('ok');
 };
 
-exports.getUser = function(req, res){
+exports.getUser = function (req, res) {
     var user = req.user.toJSON();
     delete user.password;
     res.send(user);
