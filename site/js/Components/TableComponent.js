2bed47178b195302cd8a86c8ca825b0c42fd9f0d
eran <shush6@gmail.com>
2015-08-01 10:57:58 +0300

    testing

--- site/js/Components/TableComponent.js
+++ site/js/Components/TableComponent.js
@@ -3,7 +3,7 @@ module.exports = React.createClass({
     render: function () {
         var a = ['lalala', 'kuku', 'kulululu'].map(str => <h1> str </h1>);
         return (
-            <div>{['lalala', 'kuku', 'kulululu'].map(str => str)}</div>
+            <div>{['bababababaababalalala', 'kuku', 'kulululu'].map(str => <h1>{str}</h1>)}</div>
         );
     }
 });
