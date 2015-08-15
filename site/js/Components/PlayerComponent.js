b0558a7f63afbcdcf24afea96f7f28974966ef0c
eran <shush6@gmail.com>
2015-08-15 21:15:24 +0300

    basic player component

--- site/js/Components/PlayerComponent.js
+++ site/js/Components/PlayerComponent.js
@@ -4,8 +4,15 @@ var React = require('react');
 var PlayerComponent = React.createClass({
 
     render: function() {
+        var player = this.props.player;
         return (
-            <div>{this.props.player.name}</div>
+            <div className={player.folded ? 'player player--folded' : 'player'}>
+                <div className="player__title">{player.name}</div>
+                <img className="player__image" src={player.imageUrl} />
+                <div className="player__balance">{player.balance}</div>
+                <div className="player__bet">{player.bet ? player.bet + '$' : ''}</div>
+
+            </div>
         );
     }
 
