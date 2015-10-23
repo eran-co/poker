816bdf503bb13374facad42217756f64fd871756
eran <shush6@gmail.com>
2015-10-23 14:42:19 +0300

    use react-dom for rendering

--- site/app.js
+++ site/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
 
@@ -7,8 +8,10 @@ import TableComponent from './js/Components/TableComponent.js';
 
 let store = createStore(pokerApp);
 
-React.render(
+let rootElement = document.getElementById('root');
+
+render(
     <Provider store={store}>
         <TableComponent store={store}/>
     </Provider>,
-     document.body);
+    rootElement);
